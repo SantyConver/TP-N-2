3f8c92e9bf8adcf734e0a6dd175fc2daeeeb73dc
Santy <santiconvertini@gmail.com>
2023-06-08 20:55:28 -0300

    Hay que corregir OrdernadorDeNumerosAscendentes. HoramasUnSeg terminado y inicio de DetectorCapicua

---
+++ src/HoraMasUnSeg.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class HoraMasUnSeg {
+    public static void main(String[] args) {
+        Scanner ent = new Scanner(System.in);
+
+        int horas, minutos,segundos;
+
+        System.out.print("Ingrese las horas   : ");
+        horas = ent.nextInt();
+
+        System.out.print("Ingrese los minutos : ");
+        minutos = ent.nextInt();
+
+        System.out.print("Ingrese los segundos: ");
+        segundos = ent.nextInt();
+        System.out.printf("La hora ingresada fue: [%02d:%02d:%02d]\n",horas, minutos, segundos);
+        segundos ++;
+
+        if (segundos ==60){
+
+            segundos =0;
+
+            minutos++;
+        }
+        if (minutos ==60){
+
+            minutos =0;
+
+            horas++;
+        }
+        if (horas ==24){
+
+            horas=0;
+
+
+        }
+        System.out.printf("La hora un segundo mas tarde es: [%02d:%02d:%02d]",horas, minutos, segundos);
+
+    }
+}
--- src/OrdenadorDeNumerosAscendente.java
+++ src/OrdenadorDeNumerosAscendente.java
@@ -11,15 +11,24 @@ public class OrdenadorDeNumerosAscendente {
         System.out.print("Ingrese el tercer numero : ");
         num3 = ent.nextInt();
 
-        if (num1 <num2 && num1 <num3 && num2 < num3){
+        if (num1 <=num2 && num1 <=num3 && num2 <=num3){
+
             System.out.println(num1+", "+num2+", "+num3);
+
+        }else if (num2 <= num1 && num2 <= num3 && num1 <=num3){
+
+            System.out.println(num2+", "+num1+", "+num3);
+
+        } else if (num3 <num1 && num3 <= num2 && num1 <= num2) {
+
+            System.out.println(num3+", "+num1+", "+num2);
+
+        } else{
+
+            System.out.println(num3+", "+num2+", "+num1);
         }
 
-
-
-
-
-
+        System.out.println("---------Fin Del Programa---------");
 
     }
 }
